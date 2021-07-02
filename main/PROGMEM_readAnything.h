6edaa79b9c60a0687e89fc552f9374e053231f04
Rodrigo S. Jauregui <85684526+ro-ja-ucu@users.noreply.github.com>
2021-07-02 18:23:54 -0300

    Adding comment with author data.

--- main/PROGMEM_readAnything.h
+++ main/PROGMEM_readAnything.h
@@ -1,3 +1,5 @@
+# Tomado del post por Nick Gammon aqui: http://www.gammon.com.au/forum/?id=12615&reply=3#reply3
+
 #include <Arduino.h>  // for type definitions
 
 #ifndef CLASIFICADORA_TI3_PROGMEM_READANYTHING_H
