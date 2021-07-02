090347eb6dbb8bf6d97900eb8ad24aa59847bf44
Rodrigo S. Jauregui <85684526+ro-ja-ucu@users.noreply.github.com>
2021-07-02 18:33:12 -0300

    Removed unused class, and serial.prints.

--- main/menu_control.cpp
+++ main/menu_control.cpp
@@ -6,31 +6,63 @@
 #include "display_control.h"
 #include "menu_control.h"
 
-/*MenuItem::MenuItem() :
-        id(0),
-        parentId(0),
-        siblingIndex(0),
-        action(17),
-        text{0}
-{}
-MenuItem::MenuItem(int p_id, int p_parent_id, int p_sibling_index, const char p_text[20], int p_action) :
-        id(p_id),
-        parentId(p_parent_id),
-        siblingIndex(p_sibling_index),
-        action(p_action),
-        text{0}
-{}*/
 
-/*int MenuItem::getId() const {return id;}
-int MenuItem::getParentId() const {return parentId;}
-int MenuItem::getSiblingIndex() const {return siblingIndex;}
-enum MenuActions MenuItem::getAction() const {return (enum MenuActions)action;}
-char * MenuItem::getText() const {
-  char * returnValue;
-  for (int i = 0; strlen(text); i++) returnValue[i] = text[i];
-  returnValue[strlen(text)] = 0;
-  return returnValue;
-}*/
+MenuControl::MenuControl(const MenuItemS (&p_menu)[36], DisplayControl &p_displayControl) :
+    initialized(false),
+    stateChanged(false),
+    lockDuration(0),
+    inactivityTimer(15000),
+    restDelay(50),
+    displayControl(p_displayControl),
+    menu(p_menu)
+{}
+
+void MenuControl::initialize() {
+    if (!initialized) {
+        currentMenuItemId = 2;
+        lastActivity = millis();
+
+        printToScreenMenuItem();
+
+        initialized = true;
+    }
+}
+
+void MenuControl::triggerUserAction(enum MenuUserActions p_user_action) {
+    if (initialized) {
+        if (rested()) {
+            if (!locked()) {
+                stateChanged = true;
+                lastActivity = millis();
+                switch (p_user_action) {
+                    case MenuUserActions::Previous:
+                        currentMenuItemId = getPrevSiblingId();
+                        currentAction = MenuActions::NavigateToId;
+                        break;
+                    case MenuUserActions::Next:
+                        currentMenuItemId = getNextSiblingId();
+                        currentAction = MenuActions::NavigateToId;
+                        break;
+                    case MenuUserActions::Return:
+                        currentAction = MenuActions::NavigateToId;
+                        if (canGoBack()) {
+                            currentMenuItemId = getMenuItem(currentMenuItemId).parentId;
+                        }
+                        break;
+                    default:
+                        currentAction = (MenuActions)getMenuItem(currentMenuItemId).action;
+                        if (currentAction == MenuActions::None) stateChanged = false;
+                        break;
+                }
+            }
+        }
+    }
+}
+
+bool MenuControl::locked() { if (lockDuration == 0) return false; return (millis() >= lockFrom + lockDuration); }
+bool MenuControl::rested() { return (millis() >= lastActivity + restDelay); }
+bool MenuControl::inactive() { if (inactivityTimer == 0) return false; return (millis() >= lastActivity + inactivityTimer); }
+bool MenuControl::canGoBack() { return (getMenuItem(currentMenuItemId).parentId != 1); }
 
 int MenuControl::getSiblingCount() {
     int nodeCounter = 0;
@@ -84,74 +116,6 @@ int MenuControl::getPrevSiblingId() {
     }
     return getCurrentMenuItemId();
 }
-
-
-
-MenuControl::MenuControl(const MenuItemS (&p_menu)[36], DisplayControl &p_displayControl) :
-    initialized(false),
-    stateChanged(false),
-    lockDuration(0),
-    inactivityTimer(15000),
-    restDelay(50),
-    displayControl(p_displayControl),
-    menu(p_menu)
-{}
-
-void MenuControl::initialize() {
-    if (!initialized) {
-        //Serial.print(F("Initializing menu."));
-
-        currentMenuItemId = 2;
-        lastActivity = millis();
-
-        printToScreenMenuItem();
-
-        initialized = true;
-    } else {
-        //Serial.print(F("Menu already initialized."));
-    }
-}
-
-void MenuControl::triggerUserAction(enum MenuUserActions p_user_action) {
-    if (initialized) {
-        if (rested()) {
-            if (!locked()) {
-                stateChanged = true;
-                lastActivity = millis();
-                switch (p_user_action) {
-                    case MenuUserActions::Previous:
-                        currentMenuItemId = getPrevSiblingId();
-                        currentAction = MenuActions::NavigateToId;
-                        break;
-                    case MenuUserActions::Next:
-                        currentMenuItemId = getNextSiblingId();
-                        currentAction = MenuActions::NavigateToId;
-                        break;
-                    case MenuUserActions::Return:
-                        currentAction = MenuActions::NavigateToId;
-                        if (canGoBack()) {
-                            currentMenuItemId = getMenuItem(currentMenuItemId).parentId;
-                        }
-                        break;
-                    default:
-                        currentAction = (MenuActions)getMenuItem(currentMenuItemId).action;
-                        if (currentAction == MenuActions::None) stateChanged = false;
-                        break;
-                }
-            } else {
-                //Serial.println(F("Menu locked."));
-            }
-        }
-    }
-    else {
-        //Serial.println(F("Menu not initialized."));
-    }
-}
-
-bool MenuControl::locked() { if (lockDuration == 0) return false; return (millis() >= lockFrom + lockDuration); }
-bool MenuControl::rested() { return (millis() >= lastActivity + restDelay); }
-bool MenuControl::inactive() { if (inactivityTimer == 0) return false; return (millis() >= lastActivity + inactivityTimer); }
-bool MenuControl::canGoBack() { return (getMenuItem(currentMenuItemId).parentId != 1); }
 
 enum MenuActions MenuControl::getCurrentAction() { return currentAction; }
 int MenuControl::getCurrentMenuItemId() { return currentMenuItemId; }
@@ -240,7 +204,5 @@ void MenuControl::processState() {
                     break;
             }
         }
-    } else {
-        //Serial.println(F("Menu not initialized."));
     }
 }
